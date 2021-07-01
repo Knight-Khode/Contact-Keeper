2cec710bbcfde5b6ad8dc6e9db7929979bb4522e
Knight Khode <elmirhamude@gmail.com>
2021-06-30 21:04:04 -0700

    routes created

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express')
+const users = require('./routes/users')
+const contacts = require('./routes/contacts')
+const auth = require('./routes/auth')
 
 const app = express()
-
-const PORT = process.env.PORT || 5000
 
 app.get('/',(req,res)=>{
     res.send('hello world')
 })
+
+//Define routes
+app.use('/api/users',users)
+app.use('/api/contacts',contacts)
+app.use('/api/auth',auth)
+
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT,()=>console.log(`Server started on ${PORT}...`))
